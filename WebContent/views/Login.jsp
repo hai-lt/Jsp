a3d29defa499da36f13bd3715e8ef20b78e6dad6
Hai Le T <hai.le@asiantech.vn>
2017-04-25 11:59:51 +0700

    First page with jsp

---
+++ WebContent/views/Login.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/JSP/login" method="post">
+		<div>
+			<label>Username</label> <input type="text" name="username">
+		</div>
+		<div>
+			<label>Password</label> <input type="password" name="password">
+		</div>
+		<% if (request.getAttribute("warning") != null) {%>
+			<label><%= request.getAttribute("warning") %></label>
+			<% request.removeAttribute("warning"); %>
+		<% } %>
+		</div>
+		<input type="submit" value="Login">
+	</form>
+</body>
+</html>
---
+++ src/login/Login.java
@@ -0,0 +1,48 @@
+package login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+	  if (isMember(request.getParameter("username"), request.getParameter("password"))) {
+	    response.sendRedirect("profile");
+	    return;
+	  };
+	  request.setAttribute("warning", "Wrong username or password");
+	  request.getRequestDispatcher("views/Login.jsp").forward(request, response);	    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	private boolean isMember(String username, String password){
+	  return username.equals("admin") && password.equals("111111");
+	}
+
+}
