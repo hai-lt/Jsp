929f2c0f6f9c0d2b74361f33898182fd8079e119
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-20 06:43:38 +0700

    Define index

--- src/controllers/PatientsReportsServlet.java
+++ src/controllers/PatientsReportsServlet.java
@@ -7,15 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.bean.PatientsReport;
+import models.bo.PatientsReportBO;
+
 @WebServlet("/patients_reports")
 public class PatientsReportsServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
+  private PatientsReportBO bo;
 
   public PatientsReportsServlet() {
     super();
+    bo = new PatientsReportBO();
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.setAttribute("patients_reports", bo.index());
     request.getRequestDispatcher("/views/patients_reports/main.jsp").forward(request, response);
   }
 
