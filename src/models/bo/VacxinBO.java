b62e2ff88ff847a4bda9596f37cb35164fa2dd6c
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-20 07:53:47 +0700

    Define constructor

--- src/models/bo/VacxinBO.java
+++ src/models/bo/VacxinBO.java
@@ -9,16 +9,21 @@ import models.dao.VacxinDAO;
 
 public class VacxinBO {
   private VacxinDAO dao;
-  
+
+  public VacxinBO() {
+    super();
+    this.dao = new VacxinDAO();
+  }
+
   public boolean delete(HttpServletRequest request) {
     HashMap<String, String> conditions = new HashMap<>();
     conditions.put(VacxinDAO.ID_KEY, (String) request.getAttribute("id"));
     return dao.delete(conditions);
   }
-  
+
   public Vacxin find(HttpServletRequest request) {
     HashMap<String, String> conditions = new HashMap<>();
-    conditions.put(VacxinDAO.ID_KEY, (String) request.getAttribute("id"));
+    conditions.put(VacxinDAO.ID_KEY, request.getParameter("id") + "");
     return dao.find(conditions);
   }
 }
