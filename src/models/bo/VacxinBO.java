d67dd5b4116669a1a43b6bb65c102415e67bdcd2
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-20 12:10:18 +0700

    Define creating a vacxin

--- src/models/bo/VacxinBO.java
+++ src/models/bo/VacxinBO.java
@@ -1,5 +1,6 @@
 package models.bo;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,15 +29,29 @@ public class VacxinBO {
   }
 
   public boolean update(HttpServletRequest request) {
+    HashMap<String, String> attributes = getVacxin(request);
+
+    HashMap<String, String> conditions = new HashMap<>();
+    conditions.put(VacxinDAO.ID, request.getParameter("id"));
+    return dao.update(attributes, conditions);
+  }
+
+  public boolean create(HttpServletRequest request) {
+    try {
+      dao.getHelper().create(getVacxin(request));
+      return true;
+    } catch (SQLException e) {
+      return false;
+    }
+  }
+
+  private HashMap<String, String> getVacxin(HttpServletRequest request) {
     HashMap<String, String> attributes = new HashMap<>();
     attributes.put(VacxinDAO.NAME, request.getParameter("name"));
     attributes.put(VacxinDAO.INJECTING_COUNTS, request.getParameter("injecting_counts"));
     attributes.put(VacxinDAO.PRICE, request.getParameter("price"));
     attributes.put(VacxinDAO.DESCRIPTION, request.getParameter("description"));
     attributes.put(VacxinDAO.SUPPLIER, request.getParameter("supplier"));
-
-    HashMap<String, String> conditions = new HashMap<>();
-    conditions.put(VacxinDAO.ID, request.getParameter("id"));
-    return dao.update(attributes, conditions);
+    return attributes;
   }
 }
